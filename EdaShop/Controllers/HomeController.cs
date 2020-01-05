e7f34ddb96a0247f832f456e8ba1e5976133bb04
vovannmsk <vovuxa2013@yandex.ru>
2020-01-05 12:36:07 +0300

    Initial commit

---
+++ EdaShop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sportmaster.Models;
+
+namespace Sportmaster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EdaShop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sportmaster.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Article { get; set; }
+        public Category CategoryId { get; set; }
+    }
+}
